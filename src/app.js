af3c478d4e21471639ebbb6c36bf55c338f213b9
InfoVac <infovac@somosicev.com>
2025-06-04 01:08:33 -0300

    Atualiza CORS para permitir frontend no Render

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Middlewares
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://infovac-api.onrender.com'],
+  origin: ['http://localhost:3000', 'https://infovac-frontend.onrender.com'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
