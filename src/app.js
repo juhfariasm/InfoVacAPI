8201565ecf21d4f1d80a2978111c222a75ddc5ae
InfoVac <infovac@somosicev.com>
2025-06-04 00:45:29 -0300

    Atualiza CORS para incluir URL do Render

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Middlewares
 app.use(cors({
-  origin: ['http://localhost:3000', '*'],
+  origin: ['http://localhost:3000', 'https://infovac-api.onrender.com'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
